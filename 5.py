4ec31e9143872b51f3cb9df958835e9d9142362c
ertaevaaiym1 <86400628+ertaevaaiym1@users.noreply.github.com>
2021-07-01 20:34:46 +0600

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def subtractProductAndSum(self, n):
-    n = input('n = ')
-    p, sum = 1, 0
-    for i in range(len(n)):
-    p = p * int(n[i])
-    sum = sum + int(n[i])
-    print(p - sum)
-        
